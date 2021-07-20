c823c4434adbec90fec5fc60c1604706a9724986
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-07-20 10:16:23 -0400

    Create shape-autoencoder.py

---
+++ shape-autoencoder.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1373]:
+
+
+import tensorflow as tf
+import numpy as np
+import nevergrad as ng
+import matplotlib.pyplot as plt
+import matplotlib
+import random
+import PIL
+from PIL import Image, ImageFont, ImageDraw
