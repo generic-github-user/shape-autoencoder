61c6a954bf0043654acbb6d0b844873c560a1fc5
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-07-20 10:23:57 -0400

    Add helper function for quickly displaying data with Pyplot

--- shape-autoencoder.py
+++ shape-autoencoder.py
@@ -29,3 +29,25 @@ def U(a, b, c=None):
         c = len(b)
     a = a + ([None] * (c - len(a)))
     return [ai if ai else b[i] for i, ai in enumerate(a)]
+
+
+# In[363]:
+
+
+# plt.style.available
+
+
+# In[1291]:
+
+
+def show(im=None, data=[], plot_func='imshow', style='fivethirtyeight', axis='off', figure={}, **kwargs):
+    plt.close('all')
+    plt.style.use(style)
+    fig = plt.figure(**figure)
+    ax = fig.add_subplot()
+#     ax.imshow(im)
+    if im is not None:
+        data = [im]+data
+    getattr(ax, plot_func)(*data, **kwargs)
+    plt.axis(axis)
+    return ax
